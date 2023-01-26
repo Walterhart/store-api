2067ebb21c8b5a522024b4577d40894822f1092f
Walterhart <70658679+Walterhart@users.noreply.github.com>
2023-01-26 00:58:52 -0500

    Add errorhandler middleware

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const  app = express()
 const connectDB = require('./db/connect')
-
+const errorhandler = require('./middleware/errorhandler')
 const error404 = require('./middleware/error404')
 const port = process.env.PORT || 3000
 
 require('dotenv').config()
 
+// parse json
+app.use(express.json())
+
+// root
+
+app.get('/',(req, res) =>{
+    res.send('<h1>Store API<h1><a href = "api/v1/products"> Product<a/>')
+})
+
 // error 404 
 app.use(error404)
+
+app.use(errorhandler)
 
 // spin up server
 const start = async() =>{
---
+++ middleware/errorhandler.js
@@ -0,0 +1,6 @@
+const errorhandler = async (err, req, res, next) =>{
+    console.log(err)
+    res.status(500).json({msg: "Something went wrong, please try again "})
+}
+
+module.exports = errorhandler
