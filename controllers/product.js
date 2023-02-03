ccee0a674c1618bb74df60de7e9b4c950bfcfdf9
Walterhart <walterwhart1@gmail.com>
2023-02-03 05:15:16 -0500

    Add numeric filter

--- controllers/product.js
+++ controllers/product.js
@@ -2,15 +2,14 @@ const Product = require('../modules/product')
 
 // test function
 const getAllProductsStatic = async(req, res) =>{
-    const search ='ab'
-    const products = await Product.find({ }).sort('name').limit(4)
+    const products = await Product.find({price: {$gt:30} }).sort('name').limit(4)
     res.status(200).json({products, nbHits:products.length})
 }
 
 
 const getAllProducts = async(req, res) =>{
    
-    const {featured, company, name, sort, select} = req.query
+    const {featured, company, name, sort, select, numericFilter} = req.query
  
     const queryObject = {}
     if (featured)
@@ -29,6 +28,30 @@ const getAllProducts = async(req, res) =>{
     }
     let result =  Product.find(queryObject)
 
+     // numeric field
+     if(numericFilter){
+        const operatorMap ={
+            '>':  '$gt',
+            '>=': '$gte',
+            '=': 'eq',
+            '<': 'lt',
+            '<=': 'lte'
+        }
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilter.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+            }
+        })
+    }
+    console.log(queryObject)
+    
     // sort query if sort is a parameter
     if(sort){
       const sortList = sort.split(',').join(' ')
@@ -44,22 +67,19 @@ const getAllProducts = async(req, res) =>{
         const selectList = select.split(',').join(' ')
         result = result.select(selectList)
     }
-
+ 
     // page logic
     const page = Number(req.query.page) || 1
     const limit = Number (req.query.limit) || 10
     const skip = (page -1 ) * limit
     result = result.skip(skip).limit(limit)
-
+    
+   
     const products = await result
 
     console.log(queryObject)
     res.status(200).json({products, nbHits:products.length})
 
-}
-
-const getProduct = async(req, res) =>{
-    res.status(200).json({msg: ' a product'})
 }
 
 module.exports = { getAllProductsStatic, getAllProducts}
